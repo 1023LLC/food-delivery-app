93473ea90f5d3267bff72041bcad9f9eae76e79b
julius <nyerereoffice@gmail.com>
2024-04-16 09:37:02 +0300

    Header

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Home.css';
+import Header from '../../components/Header/Header';
 
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Header />
+    </div>
   )
 }
 
